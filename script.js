d0327e341249f389b2e50f2a6857c6d0c7e297f9
George Khoury <gkhoury17@gmail.com>
2021-03-06 11:58:09 -0700

    js file to modify css

---
+++ script.js
@@ -0,0 +1,9 @@
+var all = document.getElementsByTagName("*")
+var max = all.length
+for(var i = 0;i < max;i++){
+    //all[i].style="opacity: 0%"
+    // var bgColor = all[i].style.background-color
+    // var color = all[i].style="color"
+    // console.log(bgColor)
+    // console.log(color)
+}
